4c86a94c4ea7dc28f704814220235cecea601827
Nat-1501 <natalisoares90@gmail.com>
2022-10-21 22:44:19 -0300

    Criada a classe que vai renderizar o template

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from operator import index
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'main/index.html'
