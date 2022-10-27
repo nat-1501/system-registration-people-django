753f4cd386336c198a081045ad3b5db7c1638b64
Nat-1501 <natalisoares90@gmail.com>
2022-10-26 21:28:14 -0300

    Atualizando model

--- pessoa/models.py
+++ pessoa/models.py
@@ -2,9 +2,9 @@ from email.policy import default
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
-class Pessoa(models.model):
+class Pessoa(models.Model):
 
-    nome_completo = models.CharField(max_lenght=256)
+    nome_completo = models.CharField(max_length=256)
     data_nascimento = models.DateField(null=True)
     ativa = models.BooleanField(default=True)
     
--- pessoa/urls.py
+++ pessoa/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import  ListaPessoaView
+
 
 urlpatterns = {
-    path(' ')
+    path('', ListaPessoaView.as_view(), name='pessoa.index')
 }
